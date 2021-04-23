8c4314abb1d81b8f8c7c46936b2fd96a4ec6abbc
Martin Pisna <pisnamar@cvut.cz>
2021-04-23 12:02:38 +0200

    modified:   HW03.py added make_ack

--- utils.py
+++ utils.py
@@ -13,10 +13,24 @@ def make_packet(i, fcontent, COUNTER_LEN, MSG_LEN, CRC_LEN):
     mess = fcontent[(i-1)*MSG_LEN :((i-1)*MSG_LEN) + MSG_LEN]  # the message
 
     my_crc = str(crc32(mess))
-    while len(my_crc) < 10:
+    while len(my_crc) < CRC_LEN:
         my_crc = '0' + my_crc
     my_crc = bytes(my_crc, 'utf-8')
 
     # assemble and return packet
     mypacket = my_counter + mess + my_crc
     return mypacket
+
+def make_ack(ack, packet_num, CRC_LEN):
+    if ack == True:
+        my_ack = b"ACK"+bytes(str(packet_num), 'utf-8')
+    else: 
+        my_ack = b"RES"+bytes(str(packet_num), 'utf-8')
+
+    my_crc = str(crc32(my_ack))
+    while len(my_crc) < CRC_LEN:
+        my_crc = '0' + my_crc
+    my_crc = bytes(my_crc, 'utf-8')
+
+    my_ack += my_crc
+    return my_ack
